d90118f23a8ae08ca9f673f24c2b1d91c8830060
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-01-04 16:05:41 +0300

    Add config for compass/sass integration

---
+++ config.rb
@@ -0,0 +1,37 @@
+#
+# This file is only needed for Compass/Sass integration.
+#
+
+# Change this to :production when ready to deploy the CSS to the live server.
+environment = :development
+#environment = :production
+
+# In development, we can turn on the FireSass-compatible debug_info.
+firesass = false
+#firesass = true
+
+# Location of the theme's resources.
+css_dir         = "public/css"
+sass_dir        = "source/sass"
+images_dir      = "public/images"
+javascripts_dir = "public/js"
+
+# Require any additional compass plugins installed on your system.
+#require 'ninesixty'
+require 'zen-grids'
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = (environment == :development) ? :expanded : :compressed
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server root.
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# Pass options to sass. For development, we turn on the FireSass-compatible
+# debug_info if the firesass config variable above is true.
+sass_options = (environment == :development && firesass == true) ? {:debug_info => true} : {}
