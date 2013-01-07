0db57bdcbb0568acab7fa46f768cb8cdf4565224
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-01-07 16:59:47 +0300

    Add om-maximenu templates, add more views templates

---
+++ templates/om-maximenu-submenu-content.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file om_maximenu_submenu_content.tpl.php
+ * Default theme implementation of om maximenu contents with submenu blocks
+ *
+ * Available variables:
+ * - $content: blocks
+ *
+ * Helper variables:
+ * - $zebra: Same output as $block_zebra but independent of any block region.
+ * - $id: Same output as $block_id but independent of any block region.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ * - $user: (object) user properties
+ *
+ * @see template_preprocess_om_maximenu_submenu()
+ * @see template_preprocess_om_maximenu_submenu_links()
+ * @see template_preprocess_om_maximenu_submenu_content()
+ */
+?>  
+
+<?php if (!empty($content)): ?>
+  <div class="om-maximenu-content om-maximenu-content-nofade closed">
+    <?php print om_maximenu_content_render($content); ?>
+  </div>
+<?php endif; ?> 
+
---
+++ templates/views-view-unformatted--gallery--block.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+
+<?php 
+  $slides_max = count($rows) / 5;
+  $counter = 0;
+  //$slides_items_max = count($rows) / 5; 
+?>
+
+<?php for ($i = 0; $i < $slides_max; $i++) : ?>
+  <div class="item<?php print ($i == 0) ? ' active' : ''; ?>">
+    <?php foreach ($rows as $id => $row): ?>
+      <?php if ($id < 5 * $counter || $id >= 5 * ($counter + 1)) { continue; } ?>
+
+      <div <?php if ($classes_array[$id]) { print 'class="' . $classes_array[$id] .'"';  } ?>>
+        <?php print $row; ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+  <?php $counter++; ?>
+<? endfor; ?>
