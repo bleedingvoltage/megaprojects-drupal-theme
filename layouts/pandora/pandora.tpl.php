3c1b39fff915548c305cccc7cd44f54fafebddff
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-01-14 00:32:45 +0300

    Add "if ($content)" conditionals

--- layouts/pandora/pandora.tpl.php
+++ layouts/pandora/pandora.tpl.php
@@ -12,94 +12,146 @@
 
 <div class="panel-display panel-pandora clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-  <div class="panel-row pandora-container pandora-columns-1">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['11']; ?></div>
+  <?php if ($content['11']) : ?>
+    <div class="panel-row pandora-container pandora-columns-1">
+      <div class="panel-panel pandora-column-1">
+        <div class="inside"><?php print $content['11']; ?></div>
+      </div>
     </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-4">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['21']; ?></div>
+  <?php if ($content['21'] || $content['22'] || $content['23'] || $content['24']) : ?>
+    <div class="panel-row pandora-container pandora-columns-4">
+      <?php if ($content['21']) : ?>
+        <div class="panel-panel pandora-column-1">
+          <div class="inside"><?php print $content['21']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['22']) : ?>
+        <div class="panel-panel pandora-column-2">
+          <div class="inside"><?php print $content['22']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['23']) : ?>
+        <div class="panel-panel pandora-column-3">
+          <div class="inside"><?php print $content['23']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['24']) : ?>
+        <div class="panel-panel pandora-column-4">
+          <div class="inside"><?php print $content['24']; ?></div>
+        </div>
+      <?php endif; ?>
     </div>
-    <div class="panel-panel pandora-column-2">
-      <div class="inside"><?php print $content['22']; ?></div>
-    </div>
-    <div class="panel-panel pandora-column-3">
-      <div class="inside"><?php print $content['23']; ?></div>
-    </div>
-    <div class="panel-panel pandora-column-4">
-      <div class="inside"><?php print $content['24']; ?></div>
-    </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-3">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['31']; ?></div>
+  <?php if ($content['31'] || $content['32'] || $content['33']) : ?>
+    <div class="panel-row pandora-container pandora-columns-3">
+      <?php if ($content['31']) : ?>
+        <div class="panel-panel pandora-column-1">
+          <div class="inside"><?php print $content['31']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['32']) : ?>
+        <div class="panel-panel pandora-column-2">
+          <div class="inside"><?php print $content['32']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['33']) : ?>
+        <div class="panel-panel pandora-column-3">
+          <div class="inside"><?php print $content['33']; ?></div>
+        </div>
+      <?php endif; ?>
     </div>
-    <div class="panel-panel pandora-column-2">
-      <div class="inside"><?php print $content['32']; ?></div>
-    </div>
-    <div class="panel-panel pandora-column-3">
-      <div class="inside"><?php print $content['33']; ?></div>
-    </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-1">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['41']; ?></div>
+  <?php if ($content['41']) : ?>
+    <div class="panel-row pandora-container pandora-columns-1">
+      <div class="panel-panel pandora-column-1">
+        <div class="inside"><?php print $content['41']; ?></div>
+      </div>
     </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-3">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['51']; ?></div>
+  <?php if ($content['51'] || $content['52'] || $content['53']) : ?>
+    <div class="panel-row pandora-container pandora-columns-3">
+      <?php if ($content['51']) : ?>
+        <div class="panel-panel pandora-column-1">
+          <div class="inside"><?php print $content['51']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['52']) : ?>
+        <div class="panel-panel pandora-column-2">
+          <div class="inside"><?php print $content['52']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['53']) : ?>
+        <div class="panel-panel pandora-column-3">
+          <div class="inside"><?php print $content['53']; ?></div>
+        </div>
+      <?php endif; ?>
     </div>
-    <div class="panel-panel pandora-column-2">
-      <div class="inside"><?php print $content['52']; ?></div>
-    </div>
-    <div class="panel-panel pandora-column-3">
-      <div class="inside"><?php print $content['53']; ?></div>
-    </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-1">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['61']; ?></div>
+  <?php if ($content['61']) : ?>
+    <div class="panel-row pandora-container pandora-columns-1">
+      <div class="panel-panel pandora-column-1">
+        <div class="inside"><?php print $content['61']; ?></div>
+      </div>
     </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-3">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['71']; ?></div>
+  <?php if ($content['71'] || $content['72'] || $content['73']) : ?>
+    <div class="panel-row pandora-container pandora-columns-3">
+      <?php if ($content['71']) : ?>
+        <div class="panel-panel pandora-column-1">
+          <div class="inside"><?php print $content['71']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['72']) : ?>
+        <div class="panel-panel pandora-column-2">
+          <div class="inside"><?php print $content['72']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['73']) : ?>
+        <div class="panel-panel pandora-column-3">
+          <div class="inside"><?php print $content['73']; ?></div>
+        </div>
+      <?php endif; ?>
     </div>
-    <div class="panel-panel pandora-column-2">
-      <div class="inside"><?php print $content['72']; ?></div>
-    </div>
-    <div class="panel-panel pandora-column-3">
-      <div class="inside"><?php print $content['73']; ?></div>
-    </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-4">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['81']; ?></div>
+  <?php if ($content['81'] || $content['82'] || $content['83'] || $content['84']) : ?>
+    <div class="panel-row pandora-container pandora-columns-4">
+      <?php if ($content['81']) : ?>
+        <div class="panel-panel pandora-column-1">
+          <div class="inside"><?php print $content['81']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['82']) : ?>
+        <div class="panel-panel pandora-column-2">
+          <div class="inside"><?php print $content['82']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['83']) : ?>
+        <div class="panel-panel pandora-column-3">
+          <div class="inside"><?php print $content['83']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['84']) : ?>
+        <div class="panel-panel pandora-column-4">
+          <div class="inside"><?php print $content['84']; ?></div>
+        </div>
+      <?php endif; ?>
     </div>
-    <div class="panel-panel pandora-column-2">
-      <div class="inside"><?php print $content['82']; ?></div>
-    </div>
-    <div class="panel-panel pandora-column-3">
-      <div class="inside"><?php print $content['83']; ?></div>
-    </div>
-    <div class="panel-panel pandora-column-4">
-      <div class="inside"><?php print $content['84']; ?></div>
-    </div>
-  </div>
+  <?php endif; ?>
 
-  <div class="panel-row pandora-container pandora-columns-1">
-    <div class="panel-panel pandora-column-1">
-      <div class="inside"><?php print $content['91']; ?></div>
+  <?php if ($content['91']) : ?>
+    <div class="panel-row pandora-container pandora-columns-1">
+      <div class="panel-panel pandora-column-1">
+        <div class="inside"><?php print $content['91']; ?></div>
+      </div>
     </div>
-  </div>
+  <?php endif; ?>
 
 </div>
--- layouts/westernmost/westernmost.tpl.php
+++ layouts/westernmost/westernmost.tpl.php
@@ -12,13 +12,19 @@
 
 <div class="panel-display panel-westernmost clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-  <div class="panel-row westernmost-container westernmost-columns-2">
-    <div class="panel-panel westernmost-column-1">
-      <div class="inside"><?php print $content['11']; ?></div>
+  <?php if ($content['11'] || $content['12']) : ?>
+    <div class="panel-row westernmost-container westernmost-columns-2">
+      <?php if ($content['11']) : ?>
+        <div class="panel-panel westernmost-column-1">
+          <div class="inside"><?php print $content['11']; ?></div>
+        </div>
+      <?php endif; ?>
+      <?php if ($content['12']) : ?>
+        <div class="panel-panel westernmost-column-2">
+          <div class="inside"><?php print $content['12']; ?></div>
+        </div>
+      <?php endif; ?>
     </div>
-    <div class="panel-panel westernmost-column-2">
-      <div class="inside"><?php print $content['12']; ?></div>
-    </div>
-  </div>
+  <?php endif; ?>
 
 </div>
