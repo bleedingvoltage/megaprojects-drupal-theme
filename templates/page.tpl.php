c6a2687afceee8bb8bffc40de49430cdede1cdc2
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-01-14 15:26:41 +0300

    Stylesheet updates, improvements

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -76,37 +76,39 @@
     <?php endif; ?>
     <?php print render($title_suffix); ?>
 
-    <div id="content" class="column" role="main">
-      <div class="content-inner">
-        <?php print render($page['highlighted']); ?>
-        <a id="main-content"></a>
-        <?php print $messages; ?>
-        <?php print render($tabs); ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-      </div>
-    </div><!-- /#content -->
+    <div class="main">
+      <div id="content" class="column" role="main">
+        <div class="content-inner">
+          <?php print render($page['highlighted']); ?>
+          <a id="main-content"></a>
+          <?php print $messages; ?>
+          <?php print render($tabs); ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          <?php print render($page['content']); ?>
+          <?php print $feed_icons; ?>
+        </div>
+      </div><!-- /#content -->
 
-    <div id="navigation">
-      <?php print render($page['navigation']); ?>
-    </div><!-- /#navigation -->
+      <div id="navigation">
+        <?php print render($page['navigation']); ?>
+      </div><!-- /#navigation -->
 
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
+      <?php
+        // Render the sidebars to see if there's anything in them.
+        $sidebar_first  = render($page['sidebar_first']);
+        $sidebar_second = render($page['sidebar_second']);
+      ?>
 
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside><!-- /.sidebars -->
-    <?php endif; ?>
+      <?php if ($sidebar_first || $sidebar_second): ?>
+        <aside class="sidebars">
+          <?php print $sidebar_first; ?>
+          <?php print $sidebar_second; ?>
+        </aside><!-- /.sidebars -->
+      <?php endif; ?>
+    </div>
 
   </div><!-- /#main -->
 
