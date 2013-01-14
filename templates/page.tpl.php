e530ecad05fbd10aa2e2aa53afce1a1b42b7c8d9
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2013-01-14 09:23:54 +0300

    Add sidebars to theme

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -95,6 +95,19 @@
       <?php print render($page['navigation']); ?>
     </div><!-- /#navigation -->
 
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside><!-- /.sidebars -->
+    <?php endif; ?>
+
   </div><!-- /#main -->
 
   <footer id="footer">
